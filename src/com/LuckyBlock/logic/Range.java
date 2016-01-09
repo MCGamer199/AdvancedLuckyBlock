cc18b152e7a39b0cd2deec2636e31964c7909129
MCGamer199 <minecraft1991995@hotmail.com>
2016-01-09 23:06:22 +0300

    Create Range.java

---
+++ src/com/LuckyBlock/logic/Range.java
@@ -0,0 +1,51 @@
+package com.LuckyBlock.logic;
+
+public class Range {
+	
+	
+	private int x;
+	private int y;
+	private int z;
+	
+	
+	public Range(int x, int y, int z){
+	this.x = x;
+	this.y = y;
+	this.z = z;
+	}
+	
+	public void setY(int y){
+	this.y = y;
+	}
+	
+	public void setZ(int z){
+	this.z = z;
+	}
+	
+	public void setX(int x){
+	this.x = x;
+	}
+	
+	public int getX(){
+	return x;
+	}
+	
+	public int getY(){
+	return y;
+	}
+	
+	public int getZ(){
+	return z;
+	}
+	
+	public int getTotal(){
+	return x+y+z;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
