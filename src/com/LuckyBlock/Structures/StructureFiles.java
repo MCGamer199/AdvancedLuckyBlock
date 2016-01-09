f7bac1f0b4c7b2f9451b019a99e97baa589d499e
MCGamer199 <minecraft1991995@hotmail.com>
2016-01-09 23:16:11 +0300

    Create StructureFiles.java

---
+++ src/com/LuckyBlock/Structures/StructureFiles.java
@@ -0,0 +1,51 @@
+package com.LuckyBlock.Structures;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import com.LuckyBlock.Engine.LuckyBlock;
+
+public class StructureFiles {
+	
+	protected static LuckyBlock clss = LuckyBlock.instance;
+	
+	static File luckytreeF = new File(clss.getDataFolder() + File.separator + "/Structures/LuckyTree.yml");
+	public static FileConfiguration luckytree = YamlConfiguration.loadConfiguration(luckytreeF);
+	static File luckytrapF = new File(clss.getDataFolder() + File.separator + "/Structures/LuckyTrap.yml");
+	public static FileConfiguration luckytrap = YamlConfiguration.loadConfiguration(luckytreeF);
+	static File pumpkintowerF = new File(clss.getDataFolder() + File.separator + "/Structures/PumpkinTower.yml");
+	public static FileConfiguration pumpkintower = YamlConfiguration.loadConfiguration(luckytreeF);
+	static File luckydungeonF = new File(clss.getDataFolder() + File.separator + "/Structures/LuckyDungeon.yml");
+	public static FileConfiguration luckydungeon = YamlConfiguration.loadConfiguration(luckytreeF);
+	
+	
+	public static void loadFiles(){
+	LuckyTree.Tall[0] = 4;
+	LuckyTree.Tall[1] = 9;
+	luckytree.addDefault("SpawnRate", 10);
+	luckytree.addDefault("MinTall", 4);
+	luckytree.addDefault("MaxTall", 9);
+	LuckyTree.Tall[0] = luckytree.getInt("MinTall");
+	LuckyTree.Tall[1] = luckytree.getInt("MaxTall");
+	luckytrap.addDefault("SpawnRate", 98);
+	pumpkintower.addDefault("SpawnRate", 98);
+	luckydungeon.addDefault("SpawnRate", 97);
+	luckytree.options().copyDefaults(true);
+	luckytrap.options().copyDefaults(true);
+	pumpkintower.options().copyDefaults(true);
+	luckydungeon.options().copyDefaults(true);
+	try {
+	luckytree.save(luckytreeF);
+	luckytrap.save(luckytrapF);
+	pumpkintower.save(pumpkintowerF);
+	luckydungeon.save(luckydungeonF);
+	} catch (IOException e){
+	e.printStackTrace();
+	}
+	}
+	
+	
+}
