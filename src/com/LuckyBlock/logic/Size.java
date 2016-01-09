208602aa2e94bc5b0711ada59a3011f19df6a6f1
MCGamer199 <minecraft1991995@hotmail.com>
2016-01-09 23:06:38 +0300

    Create Size.java

---
+++ src/com/LuckyBlock/logic/Size.java
@@ -0,0 +1,32 @@
+package com.LuckyBlock.logic;
+
+public class Size {
+	
+	
+	private int x;
+	private int y;
+	private int z;
+	
+	
+	
+	public Size(int x, int y, int z){
+	this.x = x;
+	this.y = y;
+	this.z = z;
+	}
+	
+	public int getX(){
+	return x;
+	}
+	
+	public int getY(){
+	return y;
+	}
+	
+	public int getZ(){
+	return z;
+	}
+	
+	
+	
+}
