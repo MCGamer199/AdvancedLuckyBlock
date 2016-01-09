4ba56cc37accdc5095060f3163775c8d52bdff76
MCGamer199 <minecraft1991995@hotmail.com>
2016-01-09 22:44:53 +0300

    Create Glow.java

---
+++ src/com/LuckyBlock/Enchantments/Glow.java
@@ -0,0 +1,49 @@
+package com.LuckyBlock.Enchantments;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+
+public class Glow extends Enchantment {
+	
+	public Glow(int id){
+	super(id);
+	}
+	
+	@Override
+	public boolean canEnchantItem(ItemStack item){
+	return true;
+	}
+	
+	@Override
+	public boolean conflictsWith(Enchantment other){
+	return false;
+	}
+	
+	@Override
+	public EnchantmentTarget getItemTarget(){
+	return null;
+	}
+	
+	@Override
+	public int getMaxLevel(){
+	return 10;
+	}
+	
+	@Override
+	public String getName(){
+	return "Glow";
+	}
+	
+	@Override
+	public int getId(){
+	return 23;
+	}
+	
+	@Override
+	public int getStartLevel(){
+	return 1;
+	}
+	
+	
+}
